95babc4da06358cfba427444e7d1204b8ea7f894
Overx18 <jricastica90@gmail.com>
2025-02-25 16:27:59 -0500

    Corrigiendo conexion con database

--- src/database/database.js
+++ src/database/database.js
@@ -5,10 +5,12 @@ dotenv.config();
 
 function createConnection() {
   return mysql.createConnection({
+
     host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT
   });
 }
 
