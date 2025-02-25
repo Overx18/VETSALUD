47fc4f94c3d296cc6e02707e1014285717970589
Overx18 <jricastica90@gmail.com>
2025-02-25 15:46:05 -0500

    Configurando conexion mysql

--- src/database/database.js
+++ src/database/database.js
@@ -8,7 +8,6 @@ function createConnection() {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    ssl: process.env.NODE_ENV === 'production' ? true : false
   });
 }
 
