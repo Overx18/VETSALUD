d03c78307a6ac201a323f266c49f218b89107009
Overx18 <jricastica90@gmail.com>
2025-02-25 15:30:41 -0500

    Acutalizando repositorio

--- src/database/database.js
+++ src/database/database.js
@@ -1,11 +1,14 @@
 const mysql = require('mysql2');
 
+dotenv.config();
+
 function createConnection() {
   return mysql.createConnection({
-    host: 'localhost',
-    database: 'bd_veterinaria',
-    user: 'root',
-    password: ''
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    ssl: process.env.NODE_ENV === 'production' ? true : false
   });
 }
 
